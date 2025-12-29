1db14c24a874b393d75c034e15cf9de94c41049e
Mihirnaik17 <mihir170204naik@gmil.com>
2025-12-29 13:03:26 -0500

    updated base URLs

--- client/src/auth/requests/index.js
+++ client/src/auth/requests/index.js
@@ -27,7 +27,11 @@
 
 //const baseURL = 'http://localhost:4000/auth';
 
-const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:4000/api';
+///const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:4000/auth';
+
+const baseURL = process.env.REACT_APP_API_URL 
+    ? `${process.env.REACT_APP_API_URL}/auth`
+    : 'http://localhost:4000/auth';
 
 const handleresponse =  async (response) => {
     if(!response.ok){
